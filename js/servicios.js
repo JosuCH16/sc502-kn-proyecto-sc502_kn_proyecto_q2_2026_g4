d1670477b18240a9a874e0391d2b0a9d7ac3a71f
AnderPHDEZ <144976041+AnderPHDEZ@users.noreply.github.com>
2026-07-04 18:46:08 -0600

    add service .css-js-html
    prueba

---
+++ js/servicios.js
@@ -0,0 +1,6 @@
+function solicitarCita(servicio) {
+
+    // Muestra un mensaje indicando cuál servicio seleccionó el usuario
+    alert("Ha seleccionado el servicio de: " + servicio + ". Será redirigido al módulo de citas.");
+
+}
